0c3df70c9f77373d029dff0970250009dc644817
Durjoy Dey <durjoydey77.dd@gmail.com>
2021-10-23 11:24:16 +0600

    Screenshot added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
